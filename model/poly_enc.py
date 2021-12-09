4e8181e88182aa05b986b59f41eb2532011d1be3
aqaqsubin <aqaq925120k@naver.com>
2021-12-09 15:36:56 +0900

    :construction: Poly-encoder attention 수정

--- model/poly_enc.py
+++ model/poly_enc.py
@@ -5,9 +5,13 @@ import torch.nn.functional as F
 from baseline import PCModel
 
 class PolyEncoder(PCModel):
-    def __init__(self, args, model_name, pooling_method):
+    def __init__(self, args, model_name, pooling_method, n_codes=64):
         super(PolyEncoder, self).__init__(args, model_name, pooling_method)
+        self.n_codes = n_codes
         
+        codes = torch.empty(self.n_codes, args.embed_size)
+        codes = torch.nn.init.uniform_(codes)
+        self.codes = torch.nn.Parameter(codes)
 
     def dot_product_attention(self, query, key, value, mask=None):
         r'''
@@ -30,19 +34,27 @@ class PolyEncoder(PCModel):
         return output.squeeze(1)
 
     def forward(self, query, cand):
+        batch_size = query.size(0)
+
         # Embedding
         # (batch_size, seq_len, embd_size)
-        y_ctxts = self.context_encoder(query)
+        h = self.context_encoder(query)
         y_cands = self.cand_encoder(cand)
 
         # Reducing
         # (batch_size, embd_size)
         y_cand = self.aggregate(y_cands)
-        
-        # Dot Attention
-        # (batch_size, embd_size)
-        y_ctxt = self.dot_product_attention(y_cand.unsqueeze(1), y_ctxts, y_ctxts, mask=None)
 
-        # (batch_size, 1)
-        score = torch.bmm(y_ctxt, y_cand.permute(1, 0))
-        return score
+        # Extracting m Global Features 
+        # (batch_size, n_codes, embd_size)
+        y_ctxt_i = self.dot_product_attention(self.codes.repeat(batch_size, 1, 1), h, h, mask=None)
+
+        # Context-Candidate Attention
+        # (batch_size, embd_size)
+        y_ctxt = self.dot_product_attention(y_cand.unsqueeze(1), y_ctxt_i, y_ctxt_i, mask=None)
+
+        # Scoring
+        # (batch_size, 1, 1)
+        score = torch.bmm(y_ctxt.unsqueeze(1), y_cand.unsqueeze(1).permute(0, 2, 1))
+        
+        return score.squeeze(1)
